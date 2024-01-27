bd1bb6e4c8395b061b9a86511130c197eecb0fa9
NataliaWszelaki <126349718+NataliaWszelaki@users.noreply.github.com>
2024-01-27 23:06:24 +0100

    First commit

---
+++ src/main/java/com/crud/adventuretravel/AdventureTravelApiFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.crud.adventuretravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureTravelApiFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureTravelApiFrontendApplication.class, args);
+    }
+
+}
